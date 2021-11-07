84d071f2058ac16438dd813acc6920c5de9c8aeb
Kitthaphob <91719944+Kitthaphob@users.noreply.github.com>
2021-11-07 21:16:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SideBar from './components/sidebar'
 import LogIn from './components/LogIn'
 import SignUp from './components/SignUp';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './components/Auth'
 
 
@@ -12,10 +12,10 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <Switch>
+        <Routes>
           <Route exact path="/Login" component={LogIn} />
           <Route exact path="/SignUp" component={SignUp} />
-          </Switch>
+          </Routes>
       <SideBar />
       <LogIn />
       <SignUp />
