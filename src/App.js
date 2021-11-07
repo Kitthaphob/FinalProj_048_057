f96a77046c077b29544870f7dfd715c49003ef3d
Kitthaphob <karnhotza@outlook.co.th>
2021-11-07 20:50:50 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import SideBar from './components/sidebar'
+import LogIn from './components/LogIn'
+import SignUp from './components/SignUp';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { AuthProvider } from './components/Auth'
+
+
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/Login" component={LogIn} />
+          <Route exact path="/SignUp" component={SignUp} />
+          </Switch>
+      <SideBar />
+      <LogIn />
+      <SignUp />
+      <LoginForm />
+      </Router>
+    </AuthProvider>
+    
+  );
+}
+
+export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app"
+import 'firebase/auth';
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyDDNY_0eqOZfEs9SAyuTcZoxRJbHWvObog",
+  authDomain: "finalproj-048-057-f2744.firebaseapp.com",
+  projectId: "finalproj-048-057-f2744",
+  storageBucket: "finalproj-048-057-f2744.appspot.com",
+  messagingSenderId: "859341262645",
+  appId: "1:859341262645:web:de76db2a3cb5a85fd5867c",
+  measurementId: "G-3JMGEYE82T"
+});
+
+export default firebaseConfig;
