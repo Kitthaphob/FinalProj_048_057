e19c08b43d87cda1c4e6b2a02a4580c58da00e03
rintori <spatanlol.25@gmail.com>
2021-11-08 22:12:35 +0700

    boot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import SideBar from './components/sidebar'
 import LogIn from './components/LogIn'
-import SignUp from './components/SignUp';
+import SignUp from './components/SignUp'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './components/Auth'
-
-
 
 function App() {
   return (
@@ -15,15 +13,14 @@ function App() {
         <Routes>
           <Route exact path="/Login" component={LogIn} />
           <Route exact path="/SignUp" component={SignUp} />
-          </Routes>
-      <SideBar />
-      <LogIn />
-      <SignUp />
+        </Routes>
+        <SideBar />
+        <LogIn />
+        <SignUp />
       </Router>
     </AuthProvider>
-    
-  );
+  )
 }
 
-export default App;
+export default App
 //aas
